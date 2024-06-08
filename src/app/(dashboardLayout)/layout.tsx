c0580453f5d1941d866ae8f6b5263216ca759fd1
alamin.147 <alamin.14780@gmail.com>
2024-06-08 17:53:15 +0600

    bug fixes

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -85,7 +85,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
               <div className="bg-gray-800 border rounded-lg px-4 py-5 sm:p-6">
                 <dl>
                   <dt className="text-sm leading-5 font-medium text-gray-400 truncate ">
-                    Total items reported
+                    Total users
                   </dt>
                   <dd className="mt-1 text-3xl leading-9 font-semibold text-indigo-400">
                     {adminStats?.data?.totalUsers}
@@ -98,75 +98,83 @@ const Layout = ({ children }: { children: ReactNode }) => {
       </div>
 
       <section className="bg-gray-900 p-3 sm:p-5 min-h-screen">
-        <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white my-10">
-            User Management
-          </h2>
-          <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-            <div className="overflow-x-auto">
-              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-4 py-3">
-                      Username
-                    </th>
-                    <th scope="col" className="px-4 py-3">
-                      Email
-                    </th>
-                    <th scope="col" className="px-4 py-3">
-                      Role
-                    </th>
-                    <th scope="col" className="px-4 py-3">
-                      Activated
-                    </th>
-                    <th scope="col" className="px-4 py-3 text-center">
-                      Block/Activate
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {adminStats?.data?.userData?.map((user: any) => {
-                    return (
-                      <tr
-                        key={user?.id}
-                        className="border-b dark:border-gray-700"
-                      >
-                        <th
-                          scope="row"
-                          className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        {users?.role == "ADMIN" ? (
+          <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white my-10">
+              User Management
+            </h2>
+            <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+              <div className="overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-4 py-3">
+                        Username
+                      </th>
+                      <th scope="col" className="px-4 py-3">
+                        Email
+                      </th>
+                      <th scope="col" className="px-4 py-3">
+                        Role
+                      </th>
+                      <th scope="col" className="px-4 py-3">
+                        Activated
+                      </th>
+                      <th scope="col" className="px-4 py-3 text-center">
+                        Block/Activate
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {adminStats?.data?.userData?.map((user: any) => {
+                      return (
+                        <tr
+                          key={user?.id}
+                          className="border-b dark:border-gray-700"
                         >
-                          {user?.username}
-                        </th>
-                        <td className="px-4 py-3">{user?.email}</td>
-                        <td className="px-4 py-3">{user?.role}</td>
-                        <td className="px-4 py-3">
-                          {user?.activated ? (
-                            <p className="text-green-400">Active</p>
-                          ) : (
-                            <p className="text-red-400">Blocked</p>
-                          )}
-                        </td>
-                        <th scope="col" className="p-4">
-                          <div className="flex items-center">
-                            {blockLoading ? (
-                              <Spinner />
+                          <th
+                            scope="row"
+                            className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                          >
+                            {user?.username}
+                          </th>
+                          <td className="px-4 py-3">{user?.email}</td>
+                          <td className="px-4 py-3">{user?.role}</td>
+                          <td className="px-4 py-3">
+                            {user?.activated ? (
+                              <p className="text-green-400">Active</p>
                             ) : (
-                              <input
-                                type="checkbox"
-                                onClick={() => handleBlock(user?.id)}
-                                className="mx-auto w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                              />
+                              <p className="text-red-400">Blocked</p>
                             )}
-                          </div>
-                        </th>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+                          </td>
+                          <th scope="col" className="p-4">
+                            <div className="flex items-center">
+                              {blockLoading ? (
+                                <Spinner />
+                              ) : (
+                                <input
+                                  type="checkbox"
+                                  onClick={() => handleBlock(user?.id)}
+                                  className="mx-auto w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                />
+                              )}
+                            </div>
+                          </th>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="mx-auto max-w-screen-xl px-4 lg:px-12 text-center">
+            <h2 className="text-3xl font-extrabold tracking-tight  sm:text-4xl text-red-400 my-10">
+              Unauthorize user
+            </h2>
+          </div>
+        )}
       </section>
       <ToastContainer />
     </div>
--- src/app/redux/api/baseApi.ts
+++ src/app/redux/api/baseApi.ts
@@ -4,7 +4,8 @@ import { getUserLocalStorage } from "@/app/auth/auth";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://asgnlast.vercel.app/api",
+    // baseUrl: "http://localhost:5000/api",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = getUserLocalStorage();
